2713a6780d6b7d404b102a4b5bfd1ddf2cf7f8db
Tomáš Rouček <rouceto1@fel.cvut.cz>
2021-03-11 18:35:18 +0100

    First navigator

---
+++ src/nav/navigator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+class Navigator:
+
+	def __init__(self, ):
+        self alignment = None
+        self uncertainty = None
+        self useUncertainty = True
+        self turnGain = 0.1 #turn 0.1 rad per each pixel of error
+        self velocityGain = 1 # 1 is same speed as thought map, less is slower more is faster
+	def process(self, msg):
+
+        correction = self.alignment * turnGain # angle = px * angle/pixel
+        if useUncertainty:
+            correction = correction * (1 - self.uncertainty)
+        out = Twist()
+        out.linear.x = msg.linear.x * velocityGain
+        out.linear.y = msg.linear.y * velocityGain
+        out.linear.z = msg.linear.z * velocityGain
+        out.angular.x = msg.angular.x * velocityGain 
+        out.angular.y = msg.angular.y * velocityGain
+        out.angular.z = msg.angular.z * velocityGain + correction
+		return out
+
+    def reconfig(cfg):
+        self.useUncertainty = cfg['use_uncertainty']
+        self.turnGain = cfg['turn_gain']
+        self.velocityGain = cfg['velocity_gain']
+
+    def correction(msg):
+		self.alignment = msg.alignment #Px
+        self.uncertainty = msg.uncertainty
+
---
+++ src/nav/ros-1-wrapper.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import rospy
+import navigator
+from geometry_msgs.msg import Twist
+from bearnav_2.msg import Alignment
+from bearnav_2.cfg import NavigatorConfig
+pub = None
+n = navigator.Navigator("./config.yaml")
+
+def callbackVel(msg):
+	driven = n.process(msg)
+	publisher.publish(driven)
+
+def callbackCorr(msg):
+    n.correction(msg)
+
+def callbackReconfigure(config):
+    n.reconfig(config)
+
+if __name__ == "__main__":
+    rospy.init_node("navigator")
+    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=0)
+    rospy.Subscriber("/map_vel", Twist, callback)
+    rospy.Subscriber("/correction_cmd", Alignment, callback)
+    srv = Server(NavigatorConfig, callbackReconfigure)
+    rospy.spin()
