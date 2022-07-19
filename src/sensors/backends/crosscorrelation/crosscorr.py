removed height from backends, removed cv_bridge dependency, fixed the histograms, sign in particle filtering

src/sensors/backends/crosscorrelation/crosscorr.py
@@ -4,16 +4,15 @@ import torch as t
 from torch.nn import functional as F
 from torchvision import transforms
 import rospy
-from cv_bridge import CvBridge
 import os
 from bearnav2.msg import SensorsInput
 from typing import List
 from scipy import interpolate
+import ros_numpy
 
 
 PAD = 32
 NEWTORK_DIVISION = 8.0
-RESIZE_H = int(320 // NEWTORK_DIVISION)
 RESIZE_W = int(512 // NEWTORK_DIVISION)
 
 
@@ -25,8 +24,6 @@ class CrossCorrelation(DisplacementEstimator):
         self.device = t.device("cuda") if t.cuda.is_available() else t.device("cpu")
         # init neural network
         self.to_tensor = transforms.ToTensor()
-        self.resize = transforms.Resize(RESIZE_H)
-        self.cv_parser = CvBridge()
         self.alignment_processing = False
         self.histograms = None
         self.distances_probs = None
@@ -41,6 +38,7 @@ class CrossCorrelation(DisplacementEstimator):
         return True
 
     def process_msg(self, msg):
+        # TODO: check if it's working for multiple map images
         hist = self.forward(msg.map_images.data, msg.live_images.data)      # not sure about .data here
         f = interpolate.interp1d(np.linspace(0, int(RESIZE_W * NEWTORK_DIVISION), len(hist[0])), hist[0], kind="cubic")
         interp_hist = f(np.arange(0, int(RESIZE_W * NEWTORK_DIVISION)))
@@ -74,7 +72,15 @@ class CrossCorrelation(DisplacementEstimator):
         return match_map
 
     def image_to_tensor(self, imgs):
-        image_list = [self.resize(self.to_tensor(np.array(self.cv_parser.imgmsg_to_cv2(img))).to(self.device))
+        desired_height = int(imgs[0].height * RESIZE_W / imgs[0].width)
+        image_list = [self.bgr_to_rgb(transforms.Resize(desired_height)(self.to_tensor(ros_numpy.numpify(img)).to(self.device)))
                       for img in imgs]
         stacked_tensor = t.stack(image_list)
         return stacked_tensor
+
+    def bgr_to_rgb(self, tensor):
+        # b = tensor[0, :, :].clone()
+        # r = tensor[2, :, :].clone()
+        # tensor[0, :, :] = r
+        # tensor[2, :, :] = b
+        return tensor

src/sensors/backends/siamese/siamese.py
@@ -4,17 +4,16 @@ import torch as t
 from backends.siamese.siam_model import get_parametrized_model, load_model
 from torchvision import transforms
 import rospy
-from cv_bridge import CvBridge
 import os
 from bearnav2.msg import SensorsInput, ImageList, Features
 from typing import List
 from scipy import interpolate
+import ros_numpy
 
 
 PAD = 32
 PEAK_MULT = 0.5
 NEWTORK_DIVISION = 8.0
-RESIZE_H = 320
 RESIZE_W = 512
 
 
@@ -32,8 +31,6 @@ class SiameseCNN(DisplacementEstimator, ProbabilityDistanceEstimator,
         self.model = load_model(model, os.path.join(file_path, "./model_eunord.pt")).to(self.device)
         self.model.eval()
         self.to_tensor = transforms.ToTensor()
-        self.resize = transforms.Resize(RESIZE_H)
-        self.cv_parser = CvBridge()
         self.alignment_processing = False
         self.histograms = None
         self.distances_probs = None
@@ -81,7 +78,7 @@ class SiameseCNN(DisplacementEstimator, ProbabilityDistanceEstimator,
         self.distances_probs = np.max(interp_hist, axis=1)
         ret = []
         for hist in interp_hist:
-            zeros = np.zeros(np.size(hist[0])//2)
+            zeros = np.zeros(np.size(hist)//2)
             ret.append(np.concatenate([zeros, hist, zeros]))    # siam can do only -0.5 to 0.5 img so extend both sides by sth
         self.histograms = ret
 
@@ -99,7 +96,15 @@ class SiameseCNN(DisplacementEstimator, ProbabilityDistanceEstimator,
         return hists
 
     def image_to_tensor(self, imgs):
-        image_list = [self.resize(self.to_tensor(np.array(self.cv_parser.imgmsg_to_cv2(img))).to(self.device))
+        desired_height = int(imgs[0].height * RESIZE_W / imgs[0].width)
+        image_list = [self.bgr_to_rgb(transforms.Resize(desired_height)(self.to_tensor(ros_numpy.numpify(img)).to(self.device)))
                       for img in imgs]
         stacked_tensor = t.stack(image_list)
         return stacked_tensor
+
+    def bgr_to_rgb(self, tensor):
+        # b = tensor[0, :, :].clone()
+        # r = tensor[2, :, :].clone()
+        # tensor[0, :, :] = r
+        # tensor[2, :, :] = b
+        return tensor

src/sensors/sensor_processing.py
@@ -151,7 +151,7 @@ class PF2D(SensorFusion):
 
         # sensor step -------------------------------------------------------------------------------
         # interpolate
-        self.particles[0] = np.clip(self.particles[0], dists[0], dists[-1])
+        self.particles[0] = np.clip(self.particles[0], dists[0] - 0.5, dists[-1] + 0.5)
         self.particles[1] = np.clip(self.particles[1], -1.0, 1.0)
         hist_width = np.shape(hists)[1]
         xs, ys = np.meshgrid(dists, np.linspace(-1.0, 1.0, hist_width))
@@ -197,7 +197,7 @@ class PF2D(SensorFusion):
             trans_diff = np.array(trans_cumsum_per_particle * frac_per_particle)
 
             # particles are shifted in odometry processing - thus np.zeros
-            particle_shifts = np.concatenate((np.zeros(trans_diff.shape), curr_img_diff - trans_diff), axis=1)
+            particle_shifts = np.concatenate((np.zeros(trans_diff.shape), -curr_img_diff + trans_diff), axis=1)
             moved_particles = np.transpose(self.particles) + particle_shifts +\
                               np.random.normal(loc=(0, 0),
                                                scale=(self.odom_error * traveled, self.align_error),